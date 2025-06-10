7f05a8c19b64be85ebc66aa17f9b39979d2575f2
safebuster2 <safebuster2@gmail.com>
2025-06-10 15:35:23 +0300

    TransactionTemplate вместо PlatformTransactionManager

--- src/main/java/com/example/intershop/service/TransactionManagerService.java
+++ src/main/java/com/example/intershop/service/TransactionManagerService.java
@@ -5,6 +5,7 @@ import com.example.intershop.repository.AccountRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.math.BigDecimal;
 
@@ -12,35 +13,30 @@ import java.math.BigDecimal;
 public class TransactionManagerService {
     private final PlatformTransactionManager transactionManager;
     private final AccountRepository accountRepository;
+    private final TransactionTemplate transactionTemplate;
 
     public TransactionManagerService(PlatformTransactionManager transactionManager,
                                      AccountRepository accountRepository) {
         this.transactionManager = transactionManager;
         this.accountRepository = accountRepository;
+        this.transactionTemplate = new TransactionTemplate(transactionManager);
+        this.transactionTemplate.setIsolationLevel(TransactionDefinition.ISOLATION_READ_COMMITTED);
+        this.transactionTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
     }
 
     public void transfer(Account source, Account target, BigDecimal amount) {
-        // Открываем транзакцию с конфигурацией по умолчанию
-        var transaction = transactionManager.getTransaction(TransactionDefinition.withDefaults());
         try {
-            // Увеличиваем баланс получателя и сохраняем
-            target.setBalance(target.getBalance().add(amount));
-            accountRepository.save(target);
+            transactionTemplate.executeWithoutResult(status -> {
+                // Увеличиваем баланс получателя и сохраняем
+                target.setBalance(target.getBalance().add(amount));
+                accountRepository.save(target);
 
-            // Уменьшаем баланс отправителя и сохраняем
-            source.setBalance(source.getBalance().subtract(amount));
-            accountRepository.save(source);
-
-            // Подтверждаем коммит транзакции в случае отсутствия ошибок
-            transactionManager.commit(transaction);
+                // Уменьшаем баланс отправителя и сохраняем
+                source.setBalance(source.getBalance().subtract(amount));
+                accountRepository.save(source);
+            });
         } catch (Exception e) {
             e.printStackTrace();
-            // Делаем откат в случае любой ошибки (например, отрицательный баланс отправителя)
-            try {
-                transactionManager.rollback(transaction);
-            } catch (Exception e2) {
-                e2.printStackTrace();
-            }
         }
     }
 }

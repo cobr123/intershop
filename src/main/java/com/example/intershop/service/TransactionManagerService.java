bfb6b6b2c020b0d402ed3f1894bd65f9545d647f
safebuster2 <safebuster2@gmail.com>
2025-06-10 16:20:04 +0300

    Transactional вместо TransactionTemplate

--- src/main/java/com/example/intershop/service/TransactionManagerService.java
+++ src/main/java/com/example/intershop/service/TransactionManagerService.java
@@ -2,37 +2,27 @@ package com.example.intershop.service;
 
 import com.example.intershop.model.Account;
 import com.example.intershop.repository.AccountRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionDefinition;
-import org.springframework.transaction.support.TransactionTemplate;
 
 import java.math.BigDecimal;
 
 @Service
 public class TransactionManagerService {
-    private final PlatformTransactionManager transactionManager;
     private final AccountRepository accountRepository;
-    private final TransactionTemplate transactionTemplate;
 
-    public TransactionManagerService(PlatformTransactionManager transactionManager,
-                                     AccountRepository accountRepository) {
-        this.transactionManager = transactionManager;
+    public TransactionManagerService(AccountRepository accountRepository) {
         this.accountRepository = accountRepository;
-        this.transactionTemplate = new TransactionTemplate(transactionManager);
-        this.transactionTemplate.setIsolationLevel(TransactionDefinition.ISOLATION_READ_COMMITTED);
-        this.transactionTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
     }
 
+    @Transactional
     public void transfer(Account source, Account target, BigDecimal amount) {
-        transactionTemplate.executeWithoutResult(status -> {
-            // Увеличиваем баланс получателя и сохраняем
-            target.setBalance(target.getBalance().add(amount));
-            accountRepository.save(target);
+        // Увеличиваем баланс получателя и сохраняем
+        target.setBalance(target.getBalance().add(amount));
+        accountRepository.save(target);
 
-            // Уменьшаем баланс отправителя и сохраняем
-            source.setBalance(source.getBalance().subtract(amount));
-            accountRepository.save(source);
-        });
+        // Уменьшаем баланс отправителя и сохраняем
+        source.setBalance(source.getBalance().subtract(amount));
+        accountRepository.save(source);
     }
 }

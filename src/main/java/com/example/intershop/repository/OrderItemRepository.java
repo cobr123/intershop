773f1408fda849c3ed9659e597f826929214b55d
safebuster2 <safebuster2@gmail.com>
2025-06-18 18:45:15 +0300

    добавил coalesce(oi.count, 0) as count

--- src/main/java/com/example/intershop/repository/OrderItemRepository.java
+++ src/main/java/com/example/intershop/repository/OrderItemRepository.java
@@ -16,15 +16,15 @@ import java.util.Optional;
 public interface OrderItemRepository extends CrudRepository<OrderItem, Long>, PagingAndSortingRepository<OrderItem, Long> {
     Optional<OrderItem> findOrderItemByOrderIdAndItemId(Long orderId, Long itemId);
 
-    @Query(value = "select i.id, i.title, i.description, i.img_path, oi.count, i.price from items i left join order_items oi on oi.order_id = ?1 and i.id = oi.item_id where i.id = ?2", nativeQuery = true)
+    @Query(value = "select i.id, i.title, i.description, i.img_path, coalesce(oi.count, 0) as count, i.price from items i left join order_items oi on oi.order_id = ?1 and i.id = oi.item_id where i.id = ?2", nativeQuery = true)
     Optional<ItemUi> findByOrderIdAndItemId(Long orderId, Long itemId);
 
     @Query(value = "select i.id, i.title, i.description, i.img_path, oi.count, i.price from items i, order_items oi where oi.order_id = ?1 and i.id = oi.item_id", nativeQuery = true)
     List<ItemUi> findByOrderId(Long orderId);
 
-    @Query(value = "select i.id, i.title, i.description, i.img_path, oi.count, i.price from items i left join order_items oi on oi.item_id = i.id and oi.order_id = ?1", nativeQuery = true)
+    @Query(value = "select i.id, i.title, i.description, i.img_path, coalesce(oi.count, 0) as count, i.price from items i left join order_items oi on oi.item_id = i.id and oi.order_id = ?1", nativeQuery = true)
     Page<ItemUi> findByTitleLikeOrDescriptionLike(Long orderId, String title, String description, Pageable pageable);
 
-    @Query(value = "select i.id, i.title, i.description, i.img_path, oi.count, i.price from items i left join order_items oi on oi.item_id = i.id and oi.order_id = ?1", nativeQuery = true)
+    @Query(value = "select i.id, i.title, i.description, i.img_path, coalesce(oi.count, 0) as count, i.price from items i left join order_items oi on oi.item_id = i.id and oi.order_id = ?1", nativeQuery = true)
     Page<ItemUi> findAll(Long orderId, Pageable pageable);
 }

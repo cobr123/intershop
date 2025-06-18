2b6bf740befe4df91d9da620971bea4d683b31cb
safebuster2 <safebuster2@gmail.com>
2025-06-18 14:57:24 +0300

    WebMvcTest вместо AutoConfigureMockMvc

--- src/test/java/com/example/intershop/controller/BuyControllerTest.java
+++ src/test/java/com/example/intershop/controller/BuyControllerTest.java
@@ -1,30 +1,43 @@
 package com.example.intershop.controller;
 
-import com.example.intershop.IntershopApplicationTests;
+import com.example.intershop.model.Order;
+import com.example.intershop.model.OrderStatus;
+import com.example.intershop.service.OrderService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@Import(IntershopApplicationTests.class)
+@WebMvcTest(BuyController.class)
 public class BuyControllerTest {
+
+    @MockitoBean
+    private OrderService orderService;
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     public void testBuy() throws Exception {
+        Order order = new Order();
+        order.setId(1L);
+        order.setStatus(OrderStatus.NEW);
+
+        doReturn(order).when(orderService).findNewOrder();
+
         mockMvc.perform(post("/buy"))
                 .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl("/orders/10?newOrder=true"));
+                .andExpect(redirectedUrl("/orders/1?newOrder=true"));
+
+        verify(orderService).update(any());
     }
 
 }

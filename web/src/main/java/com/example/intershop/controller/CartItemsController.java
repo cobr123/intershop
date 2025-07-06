f9256e669f902fa1e205d29fdc073806cbc07fa1
safebuster2 <safebuster2@gmail.com>
2025-07-06 22:00:43 +0300

    добавил сообщения "Сервис платежей недоступен" и "Недостаточно средств на счёте"

--- web/src/main/java/com/example/intershop/controller/CartItemsController.java
+++ web/src/main/java/com/example/intershop/controller/CartItemsController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 
 @Controller
 @RequestMapping("/cart/items")
@@ -39,13 +41,15 @@ public class CartItemsController {
                         .map(items -> Pair.of(order.getId(), items))
                 )
                 .flatMap(pair -> api.balanceGetWithHttpInfo()
+                        .timeout(Duration.of(5, ChronoUnit.SECONDS))
                         .map(resp -> {
                             if (resp.getStatusCode().is2xxSuccessful() && resp.getBody() != null) {
                                 return Pair.of(pair, resp.getBody().getBalance());
                             } else {
-                                return Pair.of(pair, BigDecimal.ZERO);
+                                return Pair.of(pair, BigDecimal.ONE.negate());
                             }
                         })
+                        .onErrorReturn(Pair.of(pair, BigDecimal.ONE.negate()))
                 )
                 .map(pair -> {
                     var orderId = pair.getFirst().getFirst();

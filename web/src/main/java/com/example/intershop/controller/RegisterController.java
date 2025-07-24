8008525bcdb26e9bcda7887141d0cf0cec1fe816
safebuster2 <safebuster2@gmail.com>
2025-07-24 19:06:34 +0300

    добавил логин после регистрации

--- web/src/main/java/com/example/intershop/controller/RegisterController.java
+++ web/src/main/java/com/example/intershop/controller/RegisterController.java
@@ -3,7 +3,9 @@ package com.example.intershop.controller;
 import com.example.intershop.model.RegisterUserForm;
 import com.example.intershop.model.UserUi;
 import com.example.intershop.service.UserService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -11,9 +13,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebSession;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
 import java.util.Optional;
+
+import static org.springframework.security.web.server.context.WebSessionServerSecurityContextRepository.DEFAULT_SPRING_SECURITY_CONTEXT_ATTR_NAME;
 
 @Controller
 @RequestMapping("/register")
@@ -33,7 +40,7 @@ public class RegisterController {
     }
 
     @PostMapping
-    public Mono<String> postForm(Model model, RegisterUserForm form, BindingResult errors) {
+    public Mono<String> postForm(Model model, RegisterUserForm form, BindingResult errors, ServerWebExchange exchange) {
         return ReactiveSecurityContextHolder.getContext()
                 .map(Optional::of)
                 .defaultIfEmpty(Optional.empty())
@@ -50,8 +57,17 @@ public class RegisterController {
                     UserDetails newUser = User.withUsername(form.getUsername()).password(form.getPassword()).passwordEncoder(passwordEncoder::encode).build();
                     return userService.insert(new UserUi(form.getUsername(), newUser.getPassword()));
                 })
-                .map(userUi -> {
-                    return "redirect:/";
+                .flatMap(userUi -> {
+                    return exchange.getSession()
+                            .doOnNext(session -> {
+                                SecurityContextImpl securityContext = new SecurityContextImpl();
+                                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userUi.getName(), userUi.getPassword(), List.of());
+                                securityContext.setAuthentication(authentication);
+
+                                session.getAttributes().put(DEFAULT_SPRING_SECURITY_CONTEXT_ATTR_NAME, securityContext);
+                            })
+                            .flatMap(WebSession::changeSessionId)
+                            .thenReturn("redirect:/");
                 })
                 .onErrorResume(err -> {
                     if (err.getClass() == IllegalArgumentException.class) {
